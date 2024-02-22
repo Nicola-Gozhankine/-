10b9c0ffee0d7cf8dc8a1e9e7019fdbd98e6b791
Nic <nikgorozhankin@gmail.com>
2024-02-22 15:06:07 +0300

    Форма 8

--- Служба доставки/Form2.Designer.cs
+++ Служба доставки/Form2.Designer.cs
@@ -64,7 +64,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(509, 564);
+            this.button2.Location = new System.Drawing.Point(396, 549);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
